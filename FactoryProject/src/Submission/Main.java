59d431e1fe00d9ac61d851e35d00f45f9810d989
Josh Graff <Github2@joshuagraff.com>
2015-12-04 12:48:02 -0500

    Add initial files

---
+++ FactoryProject/src/Submission/Main.java
@@ -0,0 +1,15 @@
+package Submission;
+
+import DontChange.IBuilding;
+
+public class Main {
+	public static IBuilding createHiggins(){
+		// Fill in driver code here
+		return null;
+	}
+	
+	public static IBuilding createSalisbury(){
+		// Fill in driver code here
+		return null;
+	}
+}
