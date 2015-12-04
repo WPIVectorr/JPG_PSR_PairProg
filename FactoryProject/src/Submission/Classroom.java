3db6099df57b5f1878f40e3c250ada39d2c9a4a2
RB73 <psraynes@wpi.edu>
2015-12-04 16:23:01 -0500

    Added room classes

---
+++ FactoryProject/src/Submission/Classroom.java
@@ -0,0 +1,56 @@
+package Submission;
+
+import DontChange.DeskType;
+import DontChange.IClassroom;
+
+public class Classroom implements IClassroom {
+
+	@Override
+	public int roomSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isClassroom() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isLectureHall() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isComputerLab() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getRoomNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DeskType getDeskType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDeskType(DeskType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ FactoryProject/src/Submission/ComputerLab.java
@@ -0,0 +1,56 @@
+package Submission;
+
+import DontChange.ComputerType;
+import DontChange.IComputerLab;
+
+public class ComputerLab implements IComputerLab {
+
+	@Override
+	public int roomSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isClassroom() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isLectureHall() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isComputerLab() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getRoomNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ComputerType getComputerType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setComputerType(ComputerType t) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
